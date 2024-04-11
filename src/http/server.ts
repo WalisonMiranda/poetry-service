76a192de5601cbf7159af915e0e55672d10f13af
walison miranda <walisonshido@gmail.com>
2024-04-10 21:06:53 -0300

    add routes to server

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,38 +1,18 @@
 import fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
 import cookie from "@fastify/cookie";
-import swagger from "@fastify/swagger";
-import swaggerUi from "@fastify/swagger-ui";
 import cors from "@fastify/cors";
 
+import { signUp } from "./routes/sign-up";
+import { login } from "./routes/login";
 import { createPoem } from "./routes/create-poem";
 import { getPoems } from "./routes/get-poems";
 import { getPoemById } from "./routes/get-poem-by-id";
-
-interface SwaggerOptions {
-  routePrefix: string;
-  swagger: {
-    info: {
-      title: string;
-      description: string;
-      version: string;
-    };
-    externalDocs: {
-      url: string;
-      description: string;
-    };
-    host: string;
-    schemes: string[];
-    consumes: string[];
-    produces: string[];
-  };
-  uiConfig: {
-    docExpansion: string;
-    deepLinking: boolean;
-  };
-  staticCSP: boolean;
-  transformStaticCSP: (header: string) => string;
-  exposeRoute: boolean;
-}
+import { deletePoem } from "./routes/delete-poem";
+import { postLike } from "./routes/post-like";
+import { deleteLike } from "./routes/delete-like";
+import { postComment } from "./routes/post-comment";
+import { editComment } from "./routes/edit-comment";
+import { deleteComment } from "./routes/delete-comment";
 
 const app: FastifyInstance = fastify();
 
@@ -41,46 +21,21 @@ app.register(cookie, {
   hook: "onRequest",
 });
 
-app.register(swagger, {
-  routePrefix: "/docs",
-  swagger: {
-    info: {
-      title: "Poetry Documentation",
-      description: "API documentation with Swagger to use Poetry services",
-      version: "1.0.0",
-    },
-    externalDocs: {
-      url: "https://swagger.io",
-      description: "Find more info here",
-    },
-    host: "localhost:3333",
-    // schemes: ["http"],
-    consumes: ["application/json"],
-    produces: ["application/json"],
-  },
-  uiConfig: {
-    docExpansion: "none",
-    deepLinking: true,
-  },
-  staticCSP: false,
-  transformStaticCSP: (header: string) => header,
-  exposeRoute: true,
-} as SwaggerOptions);
-
-app.register(swaggerUi, {
-  routePrefix: "/docs",
-  swaggerOptions: {
-    url: "http://localhost:3333/docs/json",
-  },
-} as RouteShorthandOptions);
-
 app.register(cors, {
   origin: true,
 });
 
+app.register(signUp);
+app.register(login);
 app.register(createPoem);
 app.register(getPoems);
 app.register(getPoemById);
+app.register(deletePoem);
+app.register(postLike);
+app.register(deleteLike);
+app.register(postComment);
+app.register(editComment);
+app.register(deleteComment);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running on port 3333");
