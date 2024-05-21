4c69c806823900d8fc5917e0723950b038226cdd
Walison Miranda <46504580+WalisonMiranda@users.noreply.github.com>
2024-05-20 23:57:24 -0300

    Update get-poems.ts

--- src/http/routes/get-poems.ts
+++ src/http/routes/get-poems.ts
@@ -1,11 +1,20 @@
 import { FastifyInstance, FastifyReply } from "fastify";
-
 import { prisma } from "../../lib/prisma";
 
 export async function getPoems(app: FastifyInstance) {
-  app.get("/poems", async (_, reply: FastifyReply) => {
+  app.get("/poems", async (request, reply: FastifyReply) => {
     try {
+      const { page = 0, limit = 10 } = request.query; // Definindo valores padrão para page e limit
+
+      const totalPoems = await prisma.poem.count(); // Conta o total de poemas para calcular a paginação
+      const skip = parseInt(page) * parseInt(limit); // Calcula o número de itens a serem ignorados
+
       const poems = await prisma.poem.findMany({
+        take: parseInt(limit),
+        skip: skip,
+        orderBy: {
+          id: 'asc', // Ordena os resultados pela ID em ordem crescente
+        },
         select: {
           id: true,
           title: true,
@@ -16,12 +25,15 @@ export async function getPoems(app: FastifyInstance) {
         },
       });
 
+      const totalPages = Math.ceil(totalPoems / parseInt(limit)); // Calcula o número total de páginas
+
       return reply.status(200).send({
         poems,
+        currentPage: parseInt(page) + 1, // Retorna a página atual começando de 1
+        totalPages, // Retorna o número total de páginas
       });
     } catch (error) {
       console.log(error);
-
       return reply.status(500).send({ message: "Erro desconhecido" });
     }
   });
